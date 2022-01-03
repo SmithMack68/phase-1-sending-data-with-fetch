651401d6692dee9d40e1fce5cf194d0be2a81ca1
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-03 18:37:18 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(data){
+    return fetch('http://localhost:3000/users', {
+       method: 'POST',
+       headers:{
+           'Content-Type': 'application/json',
+           Accept: 'application/json',
+       },
+       body: JSON.stringify({
+           name: 'Steve',
+           email: 'steve@steve.com',
+       }),
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        document.querySelector('body').innerHTML = data.id
+    })
+    .catch(error => {
+        let message = 'Unauthorized Access'
+        console.log(error.message)
+        document.body.innerHTML = error.message
+       
+    });
+};
+    submitData()
+
